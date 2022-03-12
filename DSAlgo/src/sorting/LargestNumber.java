78c26b3196812da4107cfde4c590adfcc324e64e
Krishna <krishnapriya.ramshetty@zemosolabs.com>
2022-03-12 23:44:44 +0530

    Sorting application problems

---
+++ DSAlgo/src/sorting/LargestNumber.java
@@ -0,0 +1,19 @@
+package sorting;
+
+import java.util.List;
+
+/**
+ * Given an array A of non-negative integers, arrange them such that they form the largest number.
+ * Note: The result may be very large, so you need to return a string instead of an integer.
+ *
+ * 1 <= len(A) <= 100000
+ * 0 <= A[i] <= 2*109
+ *
+ * A = [3, 30, 34, 5, 9] // 9534330
+ * A = [2, 3, 9, 0] // 9320
+ */
+public class LargestNumber {
+    public String largestNumber(final List<Integer> A) {
+        return "0";
+    }
+}
